765403f72d038efbc3c3f1577ee4cb7ef16d9a20
Hirofumi Wakasugi <baenej@gmail.com>
2016-01-09 15:41:18 +0900

    Intial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: __dirname + "/src/main.jsx",
+  output: {
+    path: __dirname + "/dist",
+    filename: "like-button.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
